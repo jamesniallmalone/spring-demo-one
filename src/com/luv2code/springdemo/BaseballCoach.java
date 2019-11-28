605913cfe865ffafe0003a6756f89627d4ac7ae4
James Malone <jamesniallmalone@users.noreply.github.com>
2019-11-28 20:15:18 +0000

    Implementation of simple spring boot application which allows us to change implementation at runtime

---
+++ src/com/luv2code/springdemo/BaseballCoach.java
@@ -0,0 +1,10 @@
+package com.luv2code.springdemo;
+
+public class BaseballCoach implements Coach {
+	
+	@Override
+	public String getDailyWorkOut() {
+		return "spend 30 minutes practicing swings";
+	}
+
+}
